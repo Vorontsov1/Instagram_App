36a3a35731a51ffc6062ef90d74f820e1be2d940
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-27 14:58:29 +0300

    3_6

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -6,6 +6,8 @@ import {
   Image,
   useWindowDimensions,
   ScrollView,
+  ViewabilityConfig,
+  ViewToken,
 } from 'react-native';
 import DoublePressable from '../DoublePressable';
 import colors from '../../../src/theme/colors.ts';
@@ -21,13 +23,13 @@ const Carousel = ({ images, onDoublePress = () => {}}: ICarousel) => {
   const {width} = useWindowDimensions();
   const [activeImageIndex, setActiveImageIndex] = useState(0);
 
-  const viewabilityConfig = {
+  const viewabilityConfig: ViewabilityConfig  = {
     itemVisiblePercentThreshold: 51,
   };
 
-  const onViewableItemsChanged = useRef(({viewableItems}) => {
+  const onViewableItemsChanged = useRef(({viewableItems}: {viewableItems: Array<ViewToken>}) => {
     if (viewableItems.length > 0) {
-      setActiveImageIndex(viewableItems[0].index);
+      setActiveImageIndex(viewableItems[0].index || 0);
     }
   });
 
@@ -43,6 +45,7 @@ const Carousel = ({ images, onDoublePress = () => {}}: ICarousel) => {
         )}
         horizontal
         pagingEnabled
+        showsHorizontalScrollIndicator={false}
         onViewableItemsChanged={onViewableItemsChanged.current}
         viewabilityConfig={viewabilityConfig}
       />
