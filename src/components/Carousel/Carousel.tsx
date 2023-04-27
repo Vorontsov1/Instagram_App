5cdfc1c62ca104e19b207cfc35dcff9d216ac6c6
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-27 11:52:54 +0300

    add_dots

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,8 @@
-import {View, Text, FlatList, Image, useWindowDimensions} from 'react-native';
+import {useState} from 'react';
+import { View, Text, FlatList, Image, useWindowDimensions, ScrollView } from 'react-native';
+import colors from '../../../src/theme/colors.ts';
+
+
 
 
 interface ICarousel {
@@ -6,18 +10,43 @@ interface ICarousel {
 }
 
 const Carousel = ({ images }: ICarousel) => {
-    const {width} = useWindowDimensions();
+    const { width } = useWindowDimensions();
+    const [activeImageIndex, setActiveImageIndex] = useState(1);
 
   return (
     <View>
       <FlatList
         data={images}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={({item}) => (
           <Image source={{uri: item}} style={{width, aspectRatio: 1}} />
-              )}              
-             horizontal
-              pagingEnabled
-   />
+        )}
+        pagingEnabled
+        horizontal
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+        }}>
+        {images.map((_, index) => (
+          <View
+            key={index}
+            style={{
+              width: 10,
+              aspectRatio: 1,
+                backgroundColor:
+                    activeImageIndex === index  ? colors.primary : colors.white,
+              borderRadius: 5,
+              margin: 10,
+              marginHorizontal: 5,
+            }}
+          />
+        ))}
+      </View>
     </View>
   );
 }
