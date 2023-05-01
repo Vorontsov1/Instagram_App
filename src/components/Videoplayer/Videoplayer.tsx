9a0fe0816919e87b75f46ddcb85011789f7a5e35
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-05-01 16:50:22 +0300

    3

--- src/components/Videoplayer/Videoplayer.tsx
+++ src/components/Videoplayer/Videoplayer.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import Video from 'react-native-video';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import colors from '../../theme/colors.ts';
+
+
 
 interface IVideoPlayer {
-    uri:  string;
+  uri: string;
 }
 
-const VideoPlayer = ({ uri }: IVideoPlayer) => { 
-    return (
+const VideoPlayer = ({uri}: IVideoPlayer) => {
+  const [muted, setMuted] = useState(true);
+
+  return (
+    <View>
       <Video
-        resizeMode="contain"
+        resizeMode="cover"
         source={{uri}}
         style={styles.video}
+        repeat
+        muted={muted}
       />
-    );
-}
-
+      <Pressable style={styles.muteButton}>
+        <Ionicons
+          name={muted ? 'volume-mute' : 'volume-medium'}
+          size={14}
+          color="white"
+          onPress={() => setMuted(v => !v)}
+        />
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    video: {
-        width: '100%',
-        aspectRatio: 16 / 9,
+  video: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+  },
+  muted: {},
+    muteButton: {
+        backgroundColor: colors.black,
+        padding: 5,
+        position: 'absolute',
+        bottom: 5,
+        right: 5,
+        borderRadius: 50,
   },
 });
 
