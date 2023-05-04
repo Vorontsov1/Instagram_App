7fd09dc656f44f05e5f201e823cac40fc28f2265
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-05-04 21:18:36 +0300

    4_1

---
+++ src/navigation/index.tsx
@@ -0,0 +1,15 @@
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from '../../src/screens/HomeScreen/HomeScreen';
+
+
+
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <HomeScreen />
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;  
