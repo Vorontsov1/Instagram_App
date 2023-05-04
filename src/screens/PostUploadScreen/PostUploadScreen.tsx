a121c23af0143b86347126fea86c2765235d58b6
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-05-04 15:16:24 +0300

    2
    add_camera

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,6 +1,26 @@
+import {useEffect, useState} from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
+import {Camera} from 'expo-camera';
 
 const PostUploadScreen = () => {
+const [hasPermissions, setHasPermissions] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        const getPermissions = async () => {
+            const cameraPermission = await Camera.requestCameraPermissionsAsync();
+            const microphonePermission = await Camera.requestMicrophonePermissionsAsync();
+            setHasPermissions(cameraPermission.status === 'granted' && microphonePermission.status === 'granted');
+        };
+        getPermissions();
+    }, []);
+    
+    if (hasPermissions === null) {
+        return <Text style={{marginTop: 50}}>Loading...</Text>
+    }
+    if (hasPermissions === false) {
+        return <Text style={{marginTop: 50}}>No access to camera</Text>;
+    } 
+
   return (
     <SafeAreaView>
       <Text>Post Upload Screen</Text>
