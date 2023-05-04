81cd982c2af5f61760358e71f52089103b2898a8
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-05-04 17:15:51 +0300

    add_Buttons

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,6 +1,8 @@
 import {useEffect, useState} from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
-import {Camera} from 'expo-camera';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import colors from '../../theme/colors.ts';
+import { Camera } from 'expo-camera';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const PostUploadScreen = () => {
 const [hasPermissions, setHasPermissions] = useState<boolean | null>(null);
@@ -22,10 +24,47 @@ const [hasPermissions, setHasPermissions] = useState<boolean | null>(null);
     } 
 
   return (
-    <SafeAreaView>
-      <Text>Post Upload Screen</Text>
+    <SafeAreaView style={styles.page}>
+      <Camera style={styles.camera} />
+      <View style={[styles.buttonContainer, {top: 45}]}>
+        <MaterialIcons name="close" size={30} color={colors.white} />
+        <MaterialIcons name="flash-off" size={30} color={colors.white} />
+        <MaterialIcons name="settings" size={30} color={colors.white} />
+      </View>
+      <View style={[styles.buttonContainer, {bottom: 35}]}>
+        <MaterialIcons name="photo-library" size={30} color={colors.white} />
+        <View style={styles.circle} />
+        <MaterialIcons name="flip-camera-ios" size={30} color={colors.white} />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: colors.black,
+  },
+  camera: {
+    width: '100%',
+    aspectRatio: 3 / 4,
+    },
+    buttonContainer: {
+        position: 'absolute',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '100%',
+  },
+    circle: {
+        width: 75,
+        aspectRatio: 1,
+        backgroundColor: colors.white,
+        borderRadius: 75,
+        justifyContent: 'center',
+        alignItems: 'center'
+  },
+});
 
 export default PostUploadScreen;
