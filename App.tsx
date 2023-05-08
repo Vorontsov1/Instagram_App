714e9369cdfb9ed99b25dd1274a4f7d4534e04c3
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-05-08 15:14:03 +0300

    add-signUp

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import Navigation from './src/navigation';
 import {Amplify} from 'aws-amplify';
 import config from './src/aws-exports';
+import {withAuthenticator} from 'aws-amplify-react-native';
+
+
 
 
 Amplify.configure(config);
@@ -13,4 +16,44 @@ const App = () => {
   );
 };
 
-export default App;
+const signUpConfig = {
+  hideAllDefaults: true,
+
+  signUpFields: [
+    {
+      label: 'Full name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+      placeholder: 'Enter your full name',
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 2,
+      type: 'string',
+      placeholder: 'Enter your email',
+    },
+    {
+      label: 'Username',
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+      placeholder: 'Enter your username',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+      placeholder: 'Enter your password',
+    },
+  ],
+};
+
+
+export default withAuthenticator(App, { signUpConfig });
