1bf55b77eaf4933df62daf65f64e40b05101440f
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-05-15 16:58:08 +0300

    6_6

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
 import Navigation from './src/navigation';
+import {Linking} from 'react-native';
 import {Amplify} from 'aws-amplify';
 import config from './src/aws-exports';
 import AuthContextProvider from './src/contexts/AuthContext';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
+
+// TODO: create the code for the in-app-browser
 
 
 
+
+const urlOpener = async (url: string, redirectUrl: string) => {
+  await InAppBrowser.isAvailable();
+  const response = await InAppBrowser.openAuth(url, redirectUrl, {
+    showTitle: false,
+    enableUrlBarHiding: true,
+    enableDefaultShare: false,
+    ephemeralWebSession: false,
+  });
+
+  if (response.type === 'success') {
+    Linking.openURL(response.url);
+  }
+};
+
+const updatedConfig = {
+  ...config,
+  oauth: {
+    ...config.oauth,
+    redirectSignIn: 'myChat://',
+    redirectSignOut: 'myChat://',
+    urlOpener,
+  },
+};
 
 
 Amplify.configure(config);
